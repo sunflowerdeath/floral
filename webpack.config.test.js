d5cfa43216a8a6041457c1af1c87d10d35595f10
sunflowerdeath <sunflowerdeath@gmail.com>
2018-06-21 04:07:20 +0300

    setup circleci

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -1,1 +1,7 @@
-module.exports = require('gnoll/config/webpack')
+const baseConfig = require('gnoll/config/webpack')
+const merge = require('webpack-merge')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = merge(baseConfig, {
+	externals: [nodeExternals()]
+})
